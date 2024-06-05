6857742317c5567f62d4c7f07a538b914106770a
erikaylopez <eylopez8686@gmail.com>
2024-06-05 16:17:24 -0600

    pwa and weather images

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,35 +1,39 @@
+import React from 'react';
+
 export default function Portfolio() {
-const projects = [ 
+    const projects = [ 
         {
             title: 'Weather Dashnboard',
             description: 'Weather application to include a search function for any location. Includes 5 day forecast',
             link: 'https://erikaylopez.github.io/Weather-Dashboard/',
-            repo: 'https://github.com/erikaylopez'
+            repo: 'https://github.com/erikaylopez',
+            image: './weather.png'
         },
-
+    
         {
             title: 'Progressive Web Applications Text Editor',
             description: 'A single-page application text editor that runs in the browser with offline functionality.',
             link: 'https://github.com/erikaylopez/PWA2',
-            repo: 'https://github.com/erikaylopez'
+            repo: 'https://github.com/erikaylopez',
+            image: 'https://example.com/pwa-text-editor.png'
         },
-
+    
         {
             title: 'RegEx Tutorial',
             description: 'A Regular Expressions tutorial to specifically cover regex matching email addresses: /^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/.',
-            link: 'https://github.com/erikaylopez/Regex-Tutorial/blob/main/Develop/gist-template.md#greedy-and-lazy-match'
+            link: 'https://github.com/erikaylopez/Regex-Tutorial/blob/main/Develop/gist-template.md#greedy-and-lazy-match',
+            image: 'https://example.com/regex-tutorial.png'
         },
         {
             title: 'Model View Controller',
             description: 'The purpose of this application is to create a CMS-style blog utilizing Model-View-Controller. This application includes Sequelize and Handlebars dependencies for deployment.',
             link: 'https://github.com/erikaylopez/Model-View-Controller',
-            repo: 'https://github.com/erikaylopez'
+            repo: 'https://github.com/erikaylopez',
+            image: 'https://example.com/mvc-blog.png'
         }
-
-
     ];
-
-
+    
+    
     return (
         <>
             <h1>Portfolio Page</h1>
@@ -37,6 +41,7 @@ const projects = [
                 <div key={index}>
                     <h2>{project.title}</h2>
                     <p>{project.description}</p>
+                    <img src={project.image} alt={project.title} />
                     <a href={project.link}>Link</a>
                 </div>
             ))}
